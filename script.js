147f1fd0a24f4fbc7c8641b7824af64bff3c8760
faria-r <fariacse11@gmail.com>
2022-09-15 00:34:34 +0600

    typing time count converted in Integer Number

--- script.js
+++ script.js
@@ -132,7 +132,6 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
-
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  const timeSpentInt = parseInt(timeSpent);
+  document.getElementById("show-time").innerHTML = `${startTime ? timeSpentInt : 0} seconds`;
 }, 1000);
